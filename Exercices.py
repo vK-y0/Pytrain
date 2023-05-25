e3309e5790e52767be3e3ff837ef62231baac26f
Vincent DA CONCEICAO RAMOS <vince9322@gmail.com>
2023-05-25 11:13:04 +0200

    first exemple

---
+++ Exercices.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+
+x = float(input("la valeur de x : "))
+y = float(input("la valeur de y : "))
+
+print("x :", x)
+print("y :", y)
+
+test = ( x + y ) / 2
+
+print("Résultat :", test)
